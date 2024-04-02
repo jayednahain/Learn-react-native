d32858cab4740219cf1f38028becb03ef3215bac
jayednahian <jayednahian@yahoo.com>
2024-04-02 22:16:20 +0600

    complete - 47

--- App/Lessons/L46GenerateRandomColors.js
+++ App/Lessons/L46GenerateRandomColors.js
@@ -1,14 +1,29 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function L46GenerateRandomColors() {
+    const [colors, setColors] = useState([]);
+
+    const onPressAddColorButton = ()=>{
+        setColors([...colors,generateRandomRGB()])
+        console.warn(colors)
+    }
+
     return (
         <View>
             {/* <Text>L46GenerateRandomColors</Text> */}
-            <Button title='Add color' />
-            <View style={{height:100, width:100, backgroundColor:'rgb(0,255,0)'}}/>
+            <Button title='Add color' onPress={onPressAddColorButton} />
+            <View style={{ height: 100, width: 100, backgroundColor: generateRandomRGB() }} />
         </View>
     )
+}
+
+const generateRandomRGB = () => {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    return `rgb(${red},${green},${blue})`
 }
 
 const styles = StyleSheet.create({})
