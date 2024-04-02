617b073ef3768aa60f2f68a04187f60d417b728c
jayednahian <jayednahian@yahoo.com>
2024-04-02 21:44:14 +0600

    46-complete

---
+++ App/Lessons/L44Counter.js
@@ -0,0 +1,13 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function L44Counter() {
+  return (
+    <View>
+        <Button title='increment' onPress={()=>{}}/>
+        <Button title='decrement' onPress={()=>{}}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ App/Lessons/L46GenerateRandomColors.js
@@ -0,0 +1,14 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function L46GenerateRandomColors() {
+    return (
+        <View>
+            {/* <Text>L46GenerateRandomColors</Text> */}
+            <Button title='Add color' />
+            <View style={{height:100, width:100, backgroundColor:'rgb(0,255,0)'}}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
